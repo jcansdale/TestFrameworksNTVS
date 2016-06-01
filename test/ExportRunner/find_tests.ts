d0efe4e43c0315e9301afb87a4f304d9fd1e3195
Jamie Cansdale <jcansdale+github@gmail.com>
2016-06-01 09:41:58 +0100

    Add tests for ExportRunner find_tests.

---
+++ test/ExportRunner/find_tests.ts
@@ -0,0 +1,48 @@
+﻿import path = require("path");
+import assert = require("assert");
+import tape = require("../../ExportRunner/exportrunner.js");
+import utilities = require("../utilities");
+
+export function find_tests_OneTest_Expect1Test() {
+    let expectedName = "__TestName__";
+    let source = testSource(expectedName);
+    let expectFile = utilities.tempFile(source);
+
+    let results = utilities.findTests("exportrunner", [expectFile]);
+
+    assert.equal(results.length, 1, "check for 1 test");
+    const [{test: testName, file: testFile}] = results;
+    assert.equal(testName, expectedName);
+    assert.equal(testFile, expectFile);
+    assert.equal(1, results.length);
+}
+
+export function find_tests_TwoTestFiles_Expect2Tests() {
+    let expectedName1 = "__TestName1__";
+    let expectFile1 = utilities.tempFile(testSource(expectedName1));
+    let expectedName2 = "__TestName2__";
+    let expectFile2 = utilities.tempFile(testSource(expectedName2));
+
+    let results = utilities.findTests("exportrunner", [expectFile1, expectFile2]);
+
+    assert.equal(results.length, 2, "check for 2 tests");
+    const [{test: testName1, file: testFile1},
+        {test: testName2, file: testFile2}] = results;
+    assert.equal(testName1, expectedName1);
+    assert.equal(testFile1, expectFile1);
+    assert.equal(testName2, expectedName2);
+    assert.equal(testFile2, expectFile2);
+    assert.equal(2, results.length);
+}
+
+//export function find_tests_OneTest_LineAndColumnAtPosition1() {
+//    let testFile = utilities.tempFile(passTestSource("__TestName__"));
+
+//    let results = utilities.findTests("tape", [testFile]);
+
+//    const [{line, column}] = results;
+//    assert.equal(line, 1, "check line number");
+//    assert.equal(column, 1, "check column number");
+//}
+
+var testSource = name => `exports.${name} = function() {}`
---
+++ test/ExportRunner/target_tests.ts
@@ -0,0 +1,8 @@
+﻿import assert = require("assert");
+
+export function fail_test() {
+    assert.fail("Boom!");
+}
+
+export function pass_test() {
+}
